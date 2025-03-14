6c0b88516b0cead2e7554809245f99ed613a8c5f
andecodelima <andrelimag89@gmail.com>
2025-03-14 09:24:18 -0300

    feat(pages):news features

--- Login/modelo02/Front-End/projeto-login/src/app/services/login.service.ts
+++ Login/modelo02/Front-End/projeto-login/src/app/services/login.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginResponse } from '../Types/login-response.type';
+import { tap } from 'rxjs';
 
+// Logica enviar os dados de login para o backEnd e pegar o retorno do backEnd e salvar o token na sessão do usuario.
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) {}
+
+  login(name:string, password:string){
+    return this.httpClient.post<LoginResponse>("/login", {name, password}).pipe(
+      tap((value)=>{
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
+
 }
